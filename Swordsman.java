eadec5efb936591898a88a03f9927bb95bf89f31
Raymond Chen <chn.ray21@gmail.com>
2025-01-09 11:47:43 -0500

    Added stuff to Swordsman

--- Swordsman.java
+++ Swordsman.java
@@ -1,3 +1,11 @@
-public class Swordsman {
-  
+public class Swordsman extends Adventurer {
+  int Rage, maxRage;
+
+  public String getSpecialName() {
+    return "Rage";
+  }
+
+  public int getSpecial() {
+    return Rage;
+  }
 }
