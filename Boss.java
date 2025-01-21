5a5f86da170420c19f35262ab1c46ef0c2f3967d
Kayden Au <aukayden@gmail.com>
2025-01-20 22:41:48 -0500

    boss class oopsies

---
+++ Boss.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Boss extends Adventurer{
+    int attackPower, maxAD;
+    public Boss(String name, int hp){
+        name = "super Boss";
+        hp = 100;
+        attackPower = 10; 
+        maxAD = 50;
+      }
+    public String getSpecialName() {
+        return "Attack Power";
+    }
+ 
+    public int getSpecial() {
+        return attackPower;
+    }
+
+    public int getSpecialMax() {
+        return maxAD;
+    }
+
+    public void setSpecial(int n) {
+        attackPower = n;
+    }
+
+    public String attack(ArrayList<Adventurer> others, int n) {
+        Adventurer enemy = others.get(n);
+        double damage = (attackPower / 2) * this.damageMult();
+        enemy.applyDamage((int)damage);
+        restoreSpecial(2);
+        return this + " hit " + enemy + " for " + damage + " damage and gained 2 attack power";
+    }
+
+    public String support(ArrayList<Adventurer> others, int n) {
+        return "No allies to support!";
+    }
+
+    public String support() {
+        this.setHP(this.getHP() + 25);
+        return this + " healed itself for 25 health!";
+    }
+
+    public String specialAttack(ArrayList<Adventurer> others, int n) {
+        for (int i = 0; i < others.size(); i++) {
+            others.get(i).applyDamage(attackPower);
+        }
+        int damage = attackPower * 2;
+        return this + " attacked all enemies for " + damage + " damage! A devasting blow!";
+    }
+}
